0cde833c622eae3defb83e365103b86448f46578
Charis Williams <Charis.Williams@hyland.com>
2020-11-05 14:00:11 -0800

    [main]: implementation of service and repository getCourses methods

---
+++ CleanArch/CleanArch.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArch.Domain.Interfaces;
+using CleanArch.Domain.Models;
+using CleanArch.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CleanArch.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _context;
+
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context;
+        }
+
+        IEnumerable<Course> ICourseRepository.GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
